51894c17f0dccdf220c8e9eb55e30549924cdf8a
weedma <vydmantas.kolduba@gmail.com>
2017-03-07 21:39:44 +0200

    solved 6

--- 6.js
+++ 6.js
@@ -1,1 +1,9 @@
-console.log(`zdrf`);
+let magic = 100;
+let valueArray = [];
+for (var i = 1; i <= magic; i++) {
+	valueArray.push(i);
+}
+//console.log(2**3);
+var squareSum = valueArray.map(x=> x**2).reduce((a,b)=>a+b,0);
+var sumSquare = valueArray.reduce((a,b)=>a+b,0)**2;
+console.log(sumSquare-squareSum);
